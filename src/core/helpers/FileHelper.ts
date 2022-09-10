24b8ac7f160ddb95df9cdb92debd97504562d282
Andrew Molchanov <mr.catmanrus@gmail.com>
2022-09-11 01:53:50 +0300

    fix runtime

--- src/core/helpers/FileHelper.ts
+++ src/core/helpers/FileHelper.ts
@@ -2,7 +2,7 @@ import fs from "fs"
 import { join } from "path"
 
 export default class FileHelper {
-    static rootDir = join(__dirname, "../../..")
+    static rootDir = join(__dirname, "..")
     static runtimeDir = join(FileHelper.rootDir, "runtime")
 
     static createMissing(): void {
